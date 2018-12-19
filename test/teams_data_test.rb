4ef936ccb2f1d22465a2486076462cca475b93a8
Martin Mercer <martinmercer1@gmail.com>
2018-12-19 15:33:01 -0700

    ran simplecov and made sure we refined the testing as best we could.

--- test/teams_data_test.rb
+++ test/teams_data_test.rb
@@ -1,7 +1,11 @@
+require 'simplecov'
+SimpleCov.start
+
 require './lib/teams_data'
 require 'pry'
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/stat_tracker'
 
 class TeamsDataTest < MiniTest::Test
 
@@ -13,17 +17,18 @@ class TeamsDataTest < MiniTest::Test
   end
 
   def test_it_exists
-    teams_data = TeamsData.new(@game_path)
+    expected = 1,23,"New Jersey","Devils","NJD","/api/v1/teams/1"
+    teams_data = TeamsData.new(expected)
 
     assert_instance_of TeamsData, teams_data
   end
 
-  def test_data_file_reads_csv
+  def test_it_has_attributes
+    expected = 1,23,"New Jersey","Devils","NJD","/api/v1/teams/1"
+    teams_data = TeamsData.new(expected)
 
-    teams_data = TeamsData.new(@game_path)
-
-    expected_hash = [{"team_id"=>1, "franchiseId"=>23, "shortName"=>"New Jersey", "teamName"=>"Devils", "abbreviation"=>"NJD", "link"=>"/api/v1/teams/1"}, {"team_id"=>4, "franchiseId"=>16, "shortName"=>"Philadelphia", "teamName"=>"Flyers", "abbreviation"=>"PHI", "link"=>"/api/v1/teams/4"}, {"team_id"=>26, "franchiseId"=>14, "shortName"=>"Los Angeles", "teamName"=>"Kings", "abbreviation"=>"LAK", "link"=>"/api/v1/teams/26"}, {"team_id"=>14, "franchiseId"=>31, "shortName"=>"Tampa Bay", "teamName"=>"Lightning", "abbreviation"=>"TBL", "link"=>"/api/v1/teams/14"}, {"team_id"=>6, "franchiseId"=>6, "shortName"=>"Boston", "teamName"=>"Bruins", "abbreviation"=>"BOS", "link"=>"/api/v1/teams/6"}]
-
-    assert_equal expected_hash, teams_data.data_file
+    assert_equal 1, teams_data.team_id
+    assert_equal 23, teams_data.franchiseId
+    assert_equal "New Jersey", teams_data.shortName
   end
-  end
+end
