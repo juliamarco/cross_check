dc2eed33ff1b71e35c081d52a6ce75a79143343a
Martin Mercer <martinmercer1@gmail.com>
2018-12-19 17:53:32 -0700

    added comment from conversation with megan

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,12 +1,15 @@
 require './lib/games_data'
 require './lib/teams_data'
 require 'csv'
+require './lib/game_statistics'
 
 class StatTracker
+  include GameStatistics
+
   attr_reader :games_data,
               :teams_data
 
-  def initialize(games_data, teams_data)  
+  def initialize(games_data, teams_data)
     @games_data = games_data
     @teams_data = teams_data
   end
@@ -14,6 +17,7 @@ class StatTracker
   def self.from_csv(locations)
     games_data = CSV.read(locations[:games], headers: true, header_converters: :symbol, converters: :numeric).map do |row|
       GameData.new(row)
+      # make row manipulation happen here, so that GameData is literally only receiving the information from the CSV
     end
     teams_data = CSV.read(locations[:teams], headers: true, header_converters: :symbol, converters: :numeric).map do |row|
       TeamsData.new(row)
